d690af873d63b89dc0d6078b3daf503f86a68e9d
Arthur_V <279230581@qq.com>
2015-12-24 17:51:58 +0800

    Create SelectArea.js

---
+++ javascripts/SelectArea.js
@@ -0,0 +1,30 @@
+app.controller("SelectArea", function($scope){
+ 	  var selectpubbuf;
+ 		var selecttagbuf=[];
+ 		selectpubbuf=_.pluck(data.books, 'publisher');
+ 		_.map(data.books, function(onebook){
+ 			selecttagbuf.push(_.pluck(onebook.tags, 'name'));
+ 			return _.pluck(onebook.tags, 'name');
+ 		});
+ 		var tagbuf=[];
+ 		for (var i = 0; i < selecttagbuf.length; i++){
+ 			var arr=selecttagbuf[i];
+ 			for(var j=0;j<arr.length;j++){
+ 				tagbuf.push(arr[j]);
+ 			}
+ 		}
+ 		var sellectpub=_.uniq(selectpubbuf);
+ 		var selecttag=_.uniq(tagbuf);
+    $scope.selecttip = [
+                    {
+                        id: 0,
+                        name: '标签',
+                        interest: selecttag
+                    },
+                    {
+                        id: 1,
+                        name: '出版社',
+                        interest: sellectpub
+                    }
+                ];
+});
